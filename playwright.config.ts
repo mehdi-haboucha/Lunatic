007588f051b8e9eedafb3e3c722941494a123bb6
Mehdi <mehdi.haboucha@insee.fr>
2023-05-03 10:34:42 +0200

    test : test e2e questionnaires - tous types boucle nombre variable ou fix (min=max)

--- playwright.config.ts
+++ playwright.config.ts
@@ -44,8 +44,8 @@ export default defineConfig({
 	/* Configure projects for major browsers */
 	projects: [
 		{
-			name: 'chromium',
-			use: { ...devices['Desktop Chrome'] },
+			name: 'firefox',
+			use: { ...devices['Desktop Firefox'] },
 		},
 		...(process.env.CI
 			? [
